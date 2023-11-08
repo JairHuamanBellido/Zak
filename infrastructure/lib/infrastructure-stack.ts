52e3f2a2241e171f2bef438ecde2c59d76032675
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-11-08 17:20:34 -0500

    feat: sign-up aws lambda trigger

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,20 +1,23 @@
 import * as cdk from "aws-cdk-lib";
-import { UserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 import { CognitoConstruct } from "./constructs/cognito.construct";
+import { SignUpLambdaTriggerConstruct } from "./constructs/sign-up-trigger-lambda.construct";
+import { UserPoolOperation } from "aws-cdk-lib/aws-cognito";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ZakStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cognito = new CognitoConstruct(this, "ZakCognito");
+    const signUpLambdaTrigger = new SignUpLambdaTriggerConstruct(
+      this,
+      "ZakSignUpLambdaTrigger"
+    ).getFunction();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
-    new CognitoConstruct(this, "ZakCognito");
+    cognito.addLambdaTrigger(
+      UserPoolOperation.POST_CONFIRMATION,
+      signUpLambdaTrigger
+    );
   }
 }
