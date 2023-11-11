8cdc27e09d8cb028522e09b6dac47c72b2da56d0
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-11-10 19:52:55 -0500

    feat: sign in

---
+++ webapp/src/lib/components/auth/banner/login-banner.svelte
@@ -0,0 +1,7 @@
+<script>
+	import banner from '$lib/assets/login-banner.jpg';
+</script>
+
+<div class="w-3/5 min-w-[400px]" >
+	<img class="h-full w-full object-cover " src={banner} alt="login-banner" />
+</div>
