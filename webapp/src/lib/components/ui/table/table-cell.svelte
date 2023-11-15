28c43c25ce159cc3e73cbc70a39ec24497f47ca3
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-11-14 22:06:21 -0500

    feat(ui): list stocks purchases by user

---
+++ webapp/src/lib/components/ui/table/table-cell.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+		import { cn } from "$lib/utils/utils";
+
+	import type { HTMLTdAttributes } from "svelte/elements";
+
+	type $$Props = HTMLTdAttributes;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<td
+	class={cn("p-2 align-middle [&:has([role=checkbox])]:pr-0", className)}
+	{...$$restProps}
+>
+	<slot />
+</td>
